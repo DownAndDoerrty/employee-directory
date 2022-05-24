e88885270205045817cbfbfc7fcca83fea580608
harrydoerr <harry@helmteam.us>
2022-05-24 08:59:46 -0700

    styles: Switching to SCSS for obvious reasons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import './styles/index.css';
 import App from './components/App';
 import GraphQLClient from './utils/GraphQLClient';
+import './styles/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
